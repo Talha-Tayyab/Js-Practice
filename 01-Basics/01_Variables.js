e8b61be795339d93d4cb224410017e4af2f3ef7f
Talha Tayyab <167535657+Talha-Tayyab@users.noreply.github.com>
2024-06-11 18:03:10 +0000

    update files

---
+++ 01-Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId= 14452
+let accountemail="Talha 383"
+var accountpass ="1284"
+accountCity="Fsd"
+accountstate;
+
+//accountId =2 // Not allowed
+ accountemail ="hamza"
+ accountpass="23452"
+ accountCity="lhe"
+
+
+console.log(accountId);
+
+console.table([accountId,accountemail,accountpass,accountCity,accountstate])
